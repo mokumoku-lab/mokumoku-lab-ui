595a0e601cc50331caa96cbf92da3078b5f7c1a7
toriiiiiii <toriiiiiii1729@gmail.com>
2024-04-04 22:29:48 +0900

    Remake: make up bases.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import MokumokulabFullLogo from "@/assets/logo/mokumokulab-full-logo.png";
+import Link from "next/link";
 
 const logo = {
   width: 101,
@@ -9,42 +10,32 @@ const logoMagnification = 6;
 
 const Header = () => {
   return (
-    <header className="bg-blue-50 p-4 flex justify-center">
+    <header className="bg-white sticky top-0 z-50 pt-5 px-2 pb-2 flex justify-center">
       <div>
         <div>
           <Image
             src={MokumokulabFullLogo}
             alt="Mokumokulab Log"
+            property="true"
             width={logo.width * logoMagnification}
             height={logo.height * logoMagnification}
           ></Image>
         </div>
-        <nav className="p-2 flex justify-around mx-auto container items-center">
-          <div className="space-x-12 font-bold">
-            <a
+        <nav className="pt-3 flex justify-around mx-auto container items-center">
+          <div className="space-x-6 font-bold">
+            <Link
               href="/home"
               className="hover:text-green-200 translate-all duration-300"
             >
               ホーム
-            </a>
-            <a
-              href="/tuition"
-              className="hover:text-green-200 translate-all duration-300"
+            </Link>
+            <Link
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfAJmWtPVwwgIHfu-C7jLlpB_q_KyWOI17qMMA0vRU-dTDaYw/viewform?usp=sf_link"
+              className="after:content-['_↗'] bg-blue-500 text-white py-1 px-2 rounded-md hover:text-green-200 translate-all duration-300"
+              target="_blank"
             >
-              料金
-            </a>
-            <a
-              href="/access"
-              className="hover:text-green-200 translate-all duration-300"
-            >
-              アクセス
-            </a>
-            <a
-              href="/contact"
-              className="hover:text-green-200 translate-all duration-300"
-            >
-              お問い合わせ
-            </a>
+              体験・入会する
+            </Link>
           </div>
         </nav>
       </div>
